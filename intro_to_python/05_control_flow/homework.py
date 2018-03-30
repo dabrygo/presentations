34a6438dbada15c94cbbc248127a0f396657d0f4
Daniel <dbgodfrey89@msn.com>
2018-03-30 07:06:08 -0600

    Add to slides and homework

--- intro_to_python/05_control_flow/homework.py
+++ intro_to_python/05_control_flow/homework.py
@@ -2,7 +2,7 @@
 import random
 choices = ('rock', 'paper', 'scissors')
 bot = random.choice(choices)
-player = random.choice(choices) #'rock'
+player = 'rock' #random.choice(choices) #input().lower(); assert player in choices
 
 print(f"player chose {player} and bot chose {bot}")
 if player == bot:
@@ -29,3 +29,55 @@ def fibonacci(n):
 
 start = [fibonacci(i) for i in range(1,11)]
 print(*start, sep=', ', end='...\n')
+
+# Problem 3
+##max_guesses = 10
+##low = 1
+##high = 100
+##number = random.randint(low, high)
+##for i in range(1, max_guesses+1):
+##    guess = int(input(f"Guess a number between {low} and {high}: "))
+##    if guess > number:
+##        print("Too high")
+##    elif guess < number:
+##        print("Too low")
+##    else:
+##        print(f"You guessed my number {number} in {i} tries!")
+##        break
+##else:
+##    print(f'None of those. My number was {number}.')
+
+
+
+# Problem 4
+inventory = {'bread': 5, 'sword': 1, 'gold': 42}
+
+def take(quantity, item):
+    if not any(inventory.values()):
+        raise Exception("I'm not carrying anything")
+    amount = inventory[item]
+    if quantity > amount:
+        raise ValueError(f"Can't take {quantity} {item}; I only have {amount}")
+    inventory[item] = amount - quantity
+
+try:
+    take(1, 'kangaroo')
+except KeyError:
+    pass
+take(5, 'bread')
+try:
+    take(2, 'sword')
+except ValueError:
+    pass
+take(1, 'sword')
+take(41, 'gold')
+try:
+    take(2, 'gold')
+except Exception:
+    pass
+take(1, 'gold')
+try:
+    take(1, 'gold')
+except:
+    pass
+print(inventory)
